15a7510506ad9a52087aa6954803824f4c632b50
Zioria <offjek065@gmail.com>
2023-12-03 00:51:58 +0700

    SaveSetScenceAndPPlayer

--- Thesis/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Thesis/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -327,7 +327,7 @@ namespace StarterAssets
                 }
 
                 // Jump
-                if (_input.jump && _jumpTimeoutDelta <= 0.0f)
+                /*if (_input.jump && _jumpTimeoutDelta <= 0.0f)
                 {
                     // the square root of H * -2 * G = how much velocity needed to reach desired height
                     _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
@@ -337,7 +337,7 @@ namespace StarterAssets
                     {
                         _animator.SetBool(_animIDJump, true);
                     }
-                }
+                }*/
 
                 // jump timeout
                 if (_jumpTimeoutDelta >= 0.0f)
