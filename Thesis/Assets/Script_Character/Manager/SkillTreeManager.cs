fa3f8a98d59fdf2cac4cdcc52f8cbaa1735a4cba
ThanonGI498 <thanon.than+GI498@bumail.net>
2023-12-03 16:14:34 +0700

    Update(03-12-2023)
    -Add ToolTip for Skill And Status
    -Use new texture(Enemy)

--- Thesis/Assets/Script_Character/Manager/SkillTreeManager.cs
+++ Thesis/Assets/Script_Character/Manager/SkillTreeManager.cs
@@ -30,12 +30,12 @@ public class SkillTreeManager : MonoBehaviour
 
     private void Update()
     {
-        if (AttributeManager.Instance.AttributeLevels[0] == 20 && AttributeManager.Instance.AttributeLevels[1] == 20 && AttributeManager.Instance.AttributeLevels[2] == 20)
+        if (AttributeManager.Instance.AttributeLevels[0] >= 20 && AttributeManager.Instance.AttributeLevels[1] >= 20 && AttributeManager.Instance.AttributeLevels[2] >= 20)
         {
             skillOneBtn.interactable = true;
         }
 
-        if (AttributeManager.Instance.AttributeLevels[0] == 15 && AttributeManager.Instance.AttributeLevels[2] == 5)
+        if (AttributeManager.Instance.AttributeLevels[0] >= 15 && AttributeManager.Instance.AttributeLevels[2] >= 5 && AttributeManager.Instance.AttributeLevels[1] >= 0)
         {
             skillTwoBtn.interactable = true;
         }
