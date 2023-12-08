ce7703f3f49311361a2e690d434d45cee11ac302
ThanonGI498 <thanon.than+GI498@bumail.net>
2023-12-08 23:37:12 +0700

    Update(08-12-2023)
    -Add LootBox
    -Remove item Prefab
    -Adjust sphere collider

---
+++ Thesis/Assets/Script_Character/Manager/LootboxManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootboxManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] itemsPrefab;
+}
--- Thesis/Assets/Script_Character/ObjectScripts/ChesObjectScript/PlayerCloseObject.cs
+++ Thesis/Assets/Script_Character/ObjectScripts/ChesObjectScript/PlayerCloseObject.cs
@@ -1,9 +1,28 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCloseObject : MonoBehaviour
 {
+    [TextArea(5,5)]
+    [SerializeField] private string debugLog;
+    [SerializeField] private GameObject[] itemsPrefab;
+    [SerializeField] private int amountToSpawn;
+    private Transform[] _posItemSpawns;
+
+
+    private void Awake()
+    {
+        _posItemSpawns = transform.Find("SpawnItemHolder").GetComponentsInChildren<Transform>();
+        if (_posItemSpawns != null)
+        {
+            for (int i = 0; i < _posItemSpawns.Length; i++)
+            {
+                debugLog += _posItemSpawns[i].name + "\n";
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +31,33 @@ public class PlayerCloseObject : MonoBehaviour
             return;
         }
         Interactor.Instance.IsInteracting = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        if (!Input.GetKeyDown(KeyCode.F))
+        {
+            return;
+        }
+
+        for (int i = 0; i < amountToSpawn; i++)
+        {
+            SpawnItem();
+        }
+        Interactor.Instance.IsInteracting = false;
+        Destroy(gameObject);
+    }
+
+    private void SpawnItem()
+    {
+        GameObject prefab = itemsPrefab[Random.Range(0, itemsPrefab.Length)];
+
+        Instantiate(prefab, _posItemSpawns[Random.Range(0, _posItemSpawns.Length)].position, Quaternion.identity);
     }
 
     private void OnTriggerExit(Collider other)
