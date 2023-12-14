f55b558eebbedd2b3ca34c799811c16983d44691
Zioria <offjek065@gmail.com>
2023-12-15 01:45:03 +0700

    FixShotcutScript

--- Thesis/Assets/Script_Character/QuestScipt/Shortcutscript.cs
+++ Thesis/Assets/Script_Character/QuestScipt/Shortcutscript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,14 @@ using UnityEngine.InputSystem;
 
 public class Shortcutscript : MonoBehaviour
 {
+    public PlayerInput _input;
+
+
+    private void Awake()
+    {
+        _input.GetComponent<PlayerInput>();
+    }
+
     public void OnQuest(InputValue value)
     {
         Debug.Log("Quest");
